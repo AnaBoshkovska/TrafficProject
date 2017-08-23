b9f0e9aae013b9142cee14533eb2b262d368db6c
LjubicaKoceva <ljubicakoceva94@gmail.com>
2017-08-23 12:27:22 +0200

    Changes in HomeController and homeView

--- app/components/controllers/HomeController.js
+++ app/components/controllers/HomeController.js
@@ -5,7 +5,7 @@
 
 'use strict';
 
-angular.module('mostPopularListingsApp.home', ['ngRoute'])
+angular.module('trafficApp')
 
 // Routing configuration for this module
 .config(['$routeProvider',function($routeprovider){
@@ -14,20 +14,13 @@ angular.module('mostPopularListingsApp.home', ['ngRoute'])
 		templateUrl: 'components/views/homeView.html'
 	});
 }])
-
 // Controller definition for this module
-.controller('HomeController', ['$scope', function($scope) {
-
-	// Just a housekeeping.
-	// In the init method we are declaring all the
-	// neccesarry settings and assignments to be run once
-	// controller is invoked
-	init();
-
-	function init(){
-	
-	};
-
-	this.message = "Hello Home!";
-
+.controller('HomeController', ['$scope', 'trafficLayerService', 'html2CanvasService', 'skopjePulseService', function($scope, trafficLayerService, html2CanvasService, skopjePulseService) {
+	$scope.getTrafficLayer = function(){
+		trafficLayerService.getTrafficLayer("map");
+	}
+	$scope.getCanvas = function(){
+		html2CanvasService.html2canvas("map");
+		skopjePulseService.getSensors();
+	}
 }]);
