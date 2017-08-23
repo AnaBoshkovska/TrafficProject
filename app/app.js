bd890974f2563a6a3e719c941b172db51a4d8461
LjubicaKoceva <ljubicakoceva94@gmail.com>
2017-08-23 12:30:04 +0200

    Dependencies added for services

--- app/app.js
+++ app/app.js
@@ -1,11 +1,10 @@
 'use strict';
 
 // Defining Angular app model with all other dependent modules
-var mostPopularListingsApp = angular.module('mostPopularListingsApp',['ngRoute',
-	'mostPopularListingsApp.home','mostPopularListingsApp.about','mostPopularListingsApp.login']);
+var trafficApp = angular.module('trafficApp',['ngRoute', 'base64']);
 
-mostPopularListingsApp.config(function($routeProvider, $locationProvider, $httpProvider) {
-	
+trafficApp.config(function($routeProvider, $locationProvider, $httpProvider, $base64) {
+
 	// Declaration of the default route if neither of the controllers
 	// is supporting the request path
 	$routeProvider.otherwise({ redirectTo: '/'});
@@ -13,6 +12,8 @@ mostPopularListingsApp.config(function($routeProvider, $locationProvider, $httpP
 	// Settings for http communications
 	$httpProvider.defaults.useXDomain = true;
 	delete $httpProvider.defaults.headers.common['X-Requested-With'];
+	var auth = $base64.encode("LjubicaKoceva:trafficlayer");
+	$httpProvider.defaults.headers.common['Authorization'] = 'Basic ' + auth;
 
 	// disabling # in Angular urls
 	// $locationProvider.html5Mode({
